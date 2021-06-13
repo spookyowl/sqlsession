4240920dd86babd29b63739714dfc48bf0741b02
peter.facka <peter.facka@friday.de>
2021-06-13 22:07:14 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="sqlsession",
-    version="0.1.30",
+    version="0.1.31",
     description='Dirt simple CRUD API to access SQL databases',
     author='Peter Facka',
     url='https://github.com/spookyowl/sqlsession',
